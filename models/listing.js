4186bc8331ea7dacfd7deb73c2eaf0a55efab28c
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-03-09 23:19:43 +0530

    Review Section

--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js")
 
 const listingSchema = new Schema({
     title : {
         type : String,
-        require : true,
+        required : true,
     },
     description : String,
     image : {
         type : String,
         default : "https://media.istockphoto.com/id/922962354/vector/no-image-available-sign.jpg?s=612x612&w=0&k=20&c=xbGzQiL_UIMFDUZte1U0end0p3E8iwocIOGt_swlywE=",
-        set : (v) => v==="" 
+        set : (v) => v === "" 
         ? "https://media.istockphoto.com/id/922962354/vector/no-image-available-sign.jpg?s=612x612&w=0&k=20&c=xbGzQiL_UIMFDUZte1U0end0p3E8iwocIOGt_swlywE=" 
-        : v,    
+        : v,
     },
     price : Number,
     location : String,
     country : String,
+
+    reviews : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Review",
+        }
+    ]
 });
+
+listingSchema.post("findOneAndDelete", async(listing) =>{
+ await Review.deleteMany({reviews :{$in: listing.reviews}});
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
